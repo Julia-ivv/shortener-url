c550aa43953456095b185fef35b3846c9e763cb4
Julia Ivanova <julia_sji85@mail.ru>
2023-10-03 17:08:32 +0300

    names corrected

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -16,7 +16,7 @@ func main() {
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc(`/`, postUrl)
+	mux.HandleFunc(`/`, postURL)
 
 	err := http.ListenAndServe(":8080", mux)
 
@@ -25,21 +25,21 @@ func main() {
 	}
 }
 
-func postUrl(res http.ResponseWriter, req *http.Request) {
+func postURL(res http.ResponseWriter, req *http.Request) {
 	switch req.Method {
 	case http.MethodPost:
 		if req.URL.Path != "/" {
 			http.Error(res, "400 bad request", http.StatusBadRequest)
 			return
 		}
-		postUrl, err := io.ReadAll(req.Body)
+		postURL, err := io.ReadAll(req.Body)
 		if err != nil {
 			http.Error(res, err.Error(), http.StatusBadRequest)
 			return
 		}
 		inc++
 		shortURL := strconv.Itoa(inc)
-		urls[shortURL] = string(postUrl)
+		urls[shortURL] = string(postURL)
 		res.Header().Set("Content-Type", "text/plain")
 		res.WriteHeader(http.StatusCreated)
 		_, err = res.Write([]byte("http://" + req.Host + "/" + shortURL))
@@ -62,33 +62,3 @@ func postUrl(res http.ResponseWriter, req *http.Request) {
 		res.WriteHeader(http.StatusBadRequest)
 	}
 }
-
-/*
-Эндпоинт с методом `POST` и путём `/`. Сервер принимает в теле запроса строку URL как `text/plain` и
-возвращает ответ с кодом `201` и сокращённым URL как `text/plain`.
-    Пример запроса к серверу:
-    POST / HTTP/1.1
-    Host: localhost:8080
-    Content-Type: text/plain
-    https://practicum.yandex.ru/
-    Пример ответа от сервера:
-    HTTP/1.1 201 Created
-    Content-Type: text/plain
-    Content-Length: 30
-    http://localhost:8080/EwHXdJfB
-
-Эндпоинт с методом `GET` и путём `/{id}`, где `id` — идентификатор сокращённого URL
-	(например, `/EwHXdJfB`).
-	В случае успешной обработки запроса сервер возвращает ответ с кодом `307`
-	и оригинальным URL в HTTP-заголовке `Location`.
-    Пример запроса к серверу:
-    GET /EwHXdJfB HTTP/1.1
-    Host: localhost:8080
-    Content-Type: text/plain
-    Пример ответа от сервера:
-    HTTP/1.1 307 Temporary Redirect
-    Location: https://practicum.yandex.ru/
-
-На любой некорректный запрос сервер должен возвращать ответ с кодом `400`Bad Request.
-
-*/
